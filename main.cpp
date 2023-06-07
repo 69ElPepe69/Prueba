e3f74a1622ce8bf561e17a51ddc3e6888c334c37
69ElPepe69 <thomas.fazio.et7@gmail.com>
2023-06-07 15:00:52 -0300

    Esto es una prueba

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string nombre;
+
+cout <<"ingresa tu nombre " <<endl;
+cin >>nombre;
+
+cout <<"tu nombre es " <<nombre <<endl;
+
+	
+return 0;	
+}
